f0546cb7718c9f70f45efc82b876b47f390c3077
Scott Diekema <scott.diekema@gmail.com>
2022-02-08 10:59:59 -0500

    cloud integration

--- Scripts/modal.js
+++ Scripts/modal.js
@@ -12,6 +12,10 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
+window.onload = function() {
+  modal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
